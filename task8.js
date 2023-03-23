ae5263462e1ba2924723822b7c17816f899c3762
omkar jadhav <officialomkarjadhav@gmail.com>
2023-03-23 07:46:08 +0000

    find even & odd in range

---
+++ task8.js
@@ -0,0 +1,11 @@
+function oddevennumber(n){
+    for(let i=0;i<n;i++){
+     if(i%2===0){
+        console.log('even '+i);
+     }
+     else{
+        console.log('odd '+i)
+     }
+    }
+}
+oddevennumber(10);
